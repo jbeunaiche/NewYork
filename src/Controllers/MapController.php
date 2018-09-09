183b6e6994da07a9df1c34fe5a1ec30d65ba7b18
jbeunaiche <jbeunaiche@gmail.com>
2018-09-09 21:57:17 +0200

    District controller

--- src/Controllers/MapController.php
+++ src/Controllers/MapController.php
@@ -8,26 +8,22 @@ class MapController extends Controller
 {
     public function midtown()
     {
-        
-        
+       
         echo $this->twig->render('districts/midtown.twig');
     }
     public function central()
     {
         
-        
         echo $this->twig->render('districts/central.twig');
     }
     public function financial()
     {
-        
-        
+    
         echo $this->twig->render('districts/financial.twig');
     }
     public function brooklyn()
     {
-        
-        
+     
         echo $this->twig->render('districts/brooklyn.twig');
     }
 }
