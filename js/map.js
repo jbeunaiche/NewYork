b4ed316fbb1c0ba50a6cf3bced938eb4a14c26ad
jbeunaiche <jbeunaiche@gmail.com>
2018-09-09 21:55:51 +0200

    Initialisation de la carte

--- js/map.js
+++ js/map.js
@@ -1,17 +1,15 @@
-var Maps = { // Creation de l'objet Maps avec les propriétés et les valeurs
-    lat: 40.784457, // La Latitude
-    lng: -73.963204, // La longitude
-    markers: [], // tableau de marqueurs
-    iconDefault: "", // Icon par défault
-
-
-    // Méthode pour la carte : 
+var Maps = { 
+    lat: 40.784457, 
+    lng: -73.963204, 
+    markers: [], // 
+    iconDefault: "", 
+ 
     initMap: function () {
         map = new google.maps.Map(document.getElementById('map'), {
             center: {
                 lat: this.lat,
                 lng: this.lng
-            }, // coordonnées de la carte et le zoom
+            }, 
             zoom: 14
         });
     },
